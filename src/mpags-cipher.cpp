b39ebb38001cf447bc3d213936410bb076faffb9
zeshan-ops <zeshan-haider@hotmail.co.uk>
2021-10-28 23:45:22 +0000

    implemented a transformChar fn

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -3,6 +3,51 @@
 #include <string>
 #include <vector>
 
+//function that transforms input characters
+std::string transformChar( const char in_char ){
+    //initialising variables
+    std::string inputText;
+    //converting to uppercase
+    if (std::isalpha(in_char)) {
+            inputText += std::toupper(in_char);
+        }
+    // Transliterate digits to English words
+    switch (in_char) {
+        case '0':
+            inputText += "ZERO";
+            break;
+        case '1':
+            inputText += "ONE";
+            break;
+        case '2':
+            inputText += "TWO";
+            break;
+        case '3':
+            inputText += "THREE";
+            break;
+        case '4':
+            inputText += "FOUR";
+            break;
+        case '5':
+            inputText += "FIVE";
+            break;
+        case '6':
+            inputText += "SIX";
+            break;
+        case '7':
+            inputText += "SEVEN";
+            break;
+        case '8':
+            inputText += "EIGHT";
+            break;
+        case '9':
+            inputText += "NINE";
+            break;
+    }
+    //returning the transformed character
+    return inputText;
+}
+
 int main(int argc, char* argv[])
 {
     // Convert the command-line arguments into a more easily usable form
@@ -97,50 +142,8 @@ int main(int argc, char* argv[])
 
     // loop over each character from user input
     while (std::cin >> inputChar) {
-        // Uppercase alphabetic characters
-        if (std::isalpha(inputChar)) {
-            inputText += std::toupper(inputChar);
-            continue;
-        }
-
-        // Transliterate digits to English words
-        switch (inputChar) {
-            case '0':
-                inputText += "ZERO";
-                break;
-            case '1':
-                inputText += "ONE";
-                break;
-            case '2':
-                inputText += "TWO";
-                break;
-            case '3':
-                inputText += "THREE";
-                break;
-            case '4':
-                inputText += "FOUR";
-                break;
-            case '5':
-                inputText += "FIVE";
-                break;
-            case '6':
-                inputText += "SIX";
-                break;
-            case '7':
-                inputText += "SEVEN";
-                break;
-            case '8':
-                inputText += "EIGHT";
-                break;
-            case '9':
-                inputText += "NINE";
-                break;
-        }
-
-        // If the character isn't alphabetic or numeric, DONT add it
+        inputText += transformChar(inputChar);
     }
-
-    // Print out the transliterated text
 
     // Warn that output file option not yet implemented
     if (!outputFile.empty()) {
@@ -148,6 +151,7 @@ int main(int argc, char* argv[])
                   << "') not implemented yet, using stdout\n";
     }
 
+    // Print out the transliterated text
     std::cout << inputText << std::endl;
 
     // No requirement to return from main, but we do so for clarity
